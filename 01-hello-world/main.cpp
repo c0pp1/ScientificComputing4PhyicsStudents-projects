3ffb3f4554466eb1d2cd79f63796529051be1e85
Alberto Coppi <albertocoppi@outlook.it>
2025-03-11 19:08:24 +0100

    fix: permissions inside container + cmake experiments

--- 01-hello-world/main.cpp
+++ 01-hello-world/main.cpp
@@ -8,10 +8,38 @@ namespace po = boost::program_options;
 
 using namespace std;
 
-const string OUTPUT_DIR = "output";
-const string OUTPUT_FILE = OUTPUT_DIR + "/output_cpp.txt";
+const string DEF_OUTPUT_DIR = "output";
 
 int main(int argc, char *argv[]) {
+
+    // Define the command line options
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("outdir,o", po::value<string>(), "output directory");
+    
+    // Parse the command line options
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // Check if the help option is provided
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    // Check if the output directory is provided
+    string OUTPUT_DIR;
+    if (vm.count("outdir")) {
+        OUTPUT_DIR = vm["outdir"].as<string>();
+    }
+    else {
+        OUTPUT_DIR = DEF_OUTPUT_DIR;
+    }
+
+    const string OUTPUT_FILE = OUTPUT_DIR + "/output_cpp.txt";
+
     // Create the output directory
     system(("mkdir -p " + OUTPUT_DIR).c_str());
 
